1dc878f48c6a70a365ad240642c7b813f2113cd9
tanishkverma423 <tanishkaverma423@gmail.com>
2026-08-18 19:07:31 +0530

    Add project files

---
+++ react/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Student from './components/Student'
+
+const App = () => {
+  return (
+    <div>
+      <Student />
+      <br />
+      <Student />
+      <br />
+      <Student />
+      <br />
+    </div>
+  )
+}
+
+export default App
